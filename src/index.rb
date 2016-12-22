547b5fc990e8105ce0b46ab03f03840d3a7efd6d
Szikszai Gusztáv <guszti5@hotmail.com>
2016-12-22 13:50:48 +0100

    Working solver.

--- src/index.rb
+++ src/index.rb
@@ -1,60 +1,89 @@
-require 'dep_selector'
+require 'solve'
 require 'tmpdir'
 require 'git'
 require 'json'
 require 'git_clone_url'
 
 class Resolver
-  include DepSelector
-
   attr_reader :graph
 
-  def initialize
-    @graph = DependencyGraph.new
+  def initialize(verbose = false)
+    @graph = Solve::Graph.new
+    @verbose = verbose
+    @cache = {}
   end
 
   def add_versions_for_package(path)
+    path = fix_path(path)
+    return if @cache[path]
+
+    puts "Adding versions for: #{path}..." if @verbose
+
     repository(path)
       .tags
       .map(&:name)
       .each do |tag|
         add_version_for_package(tag, path)
       end
+
+    @cache[path] = true
   end
 
   def add_version_for_package(tag, path)
     # Create version
-    version = package(path).add_version(tag)
+    name = package_name(path)
+    version = @graph.artifact(name, tag)
 
     # Checkout tag
     repository(path).checkout(tag)
 
     # Read elm-package.json
     elm_package_json(path)['dependencies'].each do |key, value|
-      version.dependencies << Dependency.new(@graph.package(key), '= 0.1.1')
+      pkg_name = package_name(key)
+      add_versions_for_package(key)
+
+      transform_constraint(value).each do |dep|
+        version.depends(pkg_name, dep)
+      end
     end
+  rescue
+  end
+
+  def fix_path(key)
+    GitCloneUrl.parse(key)
+    key
+  rescue
+    "git@github.com:#{key}"
+  end
+
+  def transform_constraint(constraint)
+    dependencies = []
+    constraint.gsub!(/\s/,'')
+
+    match = constraint.match(/(.*)<=v/)
+    dependencies << ">= #{match[1]}" if match
+
+    match = constraint.match(/v<(.*)/)
+    dependencies << "< #{match[1]}" if match
+
+    dependencies
   end
 
   def elm_package_json(path)
-    JSON.parse(
-      File.read(
-        File.join(repository_path(path), 'elm-package.json')
-      )
-    )
-  end
-
-  def package(path)
-    name = package_name(path)
-    @graph.package(name)
+    JSON.parse(File.read(File.join(repository_path(path), 'elm-package.json')))
+  rescue
+    { 'dependencies' => [] }
   end
 
   def package_name(path)
-    GitCloneUrl.parse(path).path
+    GitCloneUrl.parse(fix_path(path)).path
   end
 
   def repository(path)
     Git.open(repository_path(path))
   rescue
+    puts "Cloning #{path}..." if @verbose
+
     Git.clone(path, repository_path(path))
   end
 
@@ -65,8 +94,24 @@ class Resolver
   def chache_dir
     File.join(Dir.home, '.elm-github-install')
   end
+
+  def self.resolve_elm_package
+    resolver = new
+
+    deps = JSON.parse(File.read('elm-package.json'))['dependencies']
+
+    constraints = deps.map do |key, value|
+      name = resolver.package_name(key)
+      resolver.add_versions_for_package(key)
+
+      resolver.transform_constraint(value).map do |c|
+        [name, c]
+      end
+    end
+    .reduce(&:+)
+
+    Solve.it!(resolver.graph, constraints)
+  end
 end
 
-resolver = Resolver.new
-resolver.add_versions_for_package('git@github.com:gdotdesign/elm-ui')
-puts resolver.graph
+puts Resolver.resolve_elm_package
